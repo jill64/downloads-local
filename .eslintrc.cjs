26c9ac407c1f5aa7ab56eb3582d425ee8d245789
jill64 <intents.turrets0h@icloud.com>
2023-09-06 20:00:18 +0900

    setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  env: {
+    es2021: true,
+    node: true
+  }
+}
